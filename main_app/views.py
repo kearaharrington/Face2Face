d0c24f7e5c5d59ca1e5c6b2ac227cd65203a0993
calvickauer <calvickauer@gmail.com>
2022-12-08 02:17:46 -0800

    create url.py in main_app

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
+# Add LoginForm to this line...
+from django.contrib.auth.forms import AuthenticationForm
+# ...and add the following line...
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
