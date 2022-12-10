c61b45969058e077009477565c6da7c02f24006f
kearaharrington <keara.harrington@gmail.com>
2022-12-10 12:11:06 -0800

    edit: Group and Message models to reference Participant instead of User

--- main_app/models.py
+++ main_app/models.py
@@ -12,14 +12,14 @@ class Participant(models.Model):
 
 class Group(models.Model):
     topic = models.CharField(max_length=100)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(Participant, on_delete=models.CASCADE)
     members = models.ManyToManyField(Participant)
     
     def __str__(self):
         return self.topic
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    sender = models.ForeignKey(Participant, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     # updated_at = models.DateTimeField(auto_now=True)
