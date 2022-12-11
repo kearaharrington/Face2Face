6038fcfc1ea68df61cea734a634cdce723b62db6
AidenValley <jhyjsh953@gmail.com>
2022-12-10 16:16:06 -0800

    feat: text_chat app built, changes in views, urls, models, etc

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 
@@ -34,3 +35,11 @@ class Video_Chat(models.Model):
     
     def __str__(self):
         return self.participants
+
+class Room(models.Model):
+    name = models.CharField(max_length=1000)
+class Message(models.Model):
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=1000000)
+    room = models.CharField(max_length=1000000)
