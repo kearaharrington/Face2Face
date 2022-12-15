7dfbe4b51a5ef1238c57d67df8e2c5ff0abe9ecc
amoreno16003 <alex.moreno161100@gmail.com>
2022-12-14 21:41:46 -0600

    Multiple users able to join stream

--- static/js/streams.js
+++ static/js/streams.js
@@ -5,9 +5,11 @@ let UID;
 
 const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
 let localTracks = []
-let remoteUser = {}
+let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+    client.on('user-published', handleUserJoined)
+
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
@@ -21,5 +23,26 @@ let joinAndDisplayLocalStream = async () => {
     localTracks[1].play(`user-${UID}`)
     await client.publish([localTracks[0], localTracks[1]])
 }
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+    if(mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if(player != null){
+            player.remove()
+        }
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                  <div><span class="username-wrapper">My Name</span></div>
+                  <div class="video-player" id="user-${user.uid}"></div>
+                  </div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
+    }
+}
+
 
 joinAndDisplayLocalStream()
